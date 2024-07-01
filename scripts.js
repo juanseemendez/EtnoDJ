852151d48ac73cf6b706c7109ee29a1d69b84114
Juan Sebastian Mendez <jsmendez_24@hotmail.com>
2024-07-01 21:52:21 +0200

    adición de los audios

--- scripts.js
+++ scripts.js
@@ -15,37 +15,50 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(buffer => audioContext.decodeAudioData(buffer));
     }
 
+
     // Función para reproducir el audio
     function playAudio(audioBuffer) {
         const source = audioContext.createBufferSource();
         source.buffer = audioBuffer;
+        source.loop = true;  // Activar el bucle
         source.connect(audioContext.destination);
         source.start();
         return source;
-    }
+    } 
 
     // Manejar clic en cada botón
-    audioButtons.forEach(button => {
+    audioButtons.forEach((button, index) => {
+        button.dataset.active = 'false';
         const audioUrl = button.getAttribute('data-audio');
         loadAudio(audioUrl).then(audioBuffer => {
-            audioSources[button.id] = audioBuffer; // Guardar el buffer de audio
+            // Guardar el buffer de audio
+            audioSources[button.id] = {
+                buffer: audioBuffer,
+                source: null  // Inicialmente no hay fuente de audio creada
+            };
 
-            button.addEventListener('click', function() {
+            button.addEventListener('click', function() {              
                 if (this.classList.contains('active')) {
                     // Detener la reproducción si ya está activo
-                    audioSources[this.id].stop();
                     this.classList.remove('active');
+                    this.dataset.active = 'false';
+                    console.log(`Button ${index + 1} deactivated`);
+                    audioSources[this.id].source.stop();
+                    audioSources[this.id].source = null;  // Limpiar la referencia a la fuente
                 } else {
                     // Reproducir el audio si no está activo
-                    playAudio(audioSources[this.id]);
+                    const source = playAudio(audioSources[this.id].buffer);
+                    audioSources[this.id].source = source;  // Guardar la referencia a la fuente
                     this.classList.add('active');
+                    this.dataset.active = 'true';
+                    console.log(`Button ${index + 1} activated`);
                 }
             });
         });
     });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('.selector');
     
     buttons.forEach((button, index) => {
@@ -64,5 +77,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
-
+*/
 
