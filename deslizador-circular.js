f7dedbbe9ae8d1072f586693c6f1623e7dcaf196
Juan Sebastian Mendez <jsmendez_24@hotmail.com>
2024-07-06 13:39:30 +0200

    Ultima version

--- deslizador-circular.js
+++ deslizador-circular.js
@@ -1,52 +1,52 @@
 class DeslizadorCircular {
     constructor(element, options = {}) {
-        this.element = element;
-        this.radius = options.radius || 30;
-        this.value = options.value !== undefined ? options.value : 0; // Valor inicial
+        this.elemento = element;
+        this.radio = options.radius || 30;
+        this.valor = options.value !== undefined ? options.value : 0; // Valor inicial
         this.id = options.id || '';
 
-        this.init();
+        this.inicializar();
     }
 
-    init() {
-        this.outerCircle = document.createElement('div');
-        this.outerCircle.classList.add('outer-circle');
+    inicializar() {
+        this.circuloExterior = document.createElement('div');
+        this.circuloExterior.classList.add('circulo-exterior');
 
-        this.innerCircle = document.createElement('div');
-        this.innerCircle.classList.add('inner-circle');
+        this.circuloInterior = document.createElement('div');
+        this.circuloInterior.classList.add('circulo-interior');
 
-        this.indicator = document.createElement('div');
-        this.indicator.classList.add('indicator');
+        this.indicador = document.createElement('div');
+        this.indicador.classList.add('indicador');
 
-        this.outerCircle.appendChild(this.innerCircle);
-        this.outerCircle.appendChild(this.indicator);
-        this.element.appendChild(this.outerCircle);
+        this.circuloExterior.appendChild(this.circuloInterior);
+        this.circuloExterior.appendChild(this.indicador);
+        this.elemento.appendChild(this.circuloExterior);
 
-        this.indicator.addEventListener('mousedown', this.startDrag.bind(this));
-        window.addEventListener('mousemove', this.drag.bind(this));
-        window.addEventListener('mouseup', this.stopDrag.bind(this));
+        this.indicador.addEventListener('mousedown', this.iniciarArrastre.bind(this));
+        window.addEventListener('mousemove', this.arrastrar.bind(this));
+        window.addEventListener('mouseup', this.detenerArrastre.bind(this));
 
-        this.draw();
-        this.emitValueChange(); // Emitir evento al inicializar
+        this.dibujar();
+        this.emitirCambioValor(); // Emitir evento al inicializar
     }
 
-    startDrag(event) {
-        this.dragging = true;
-        this.updateValue(event);
+    iniciarArrastre(event) {
+        this.arrastrando = true;
+        this.actualizarValor(event);
     }
 
-    drag(event) {
-        if (this.dragging) {
-            this.updateValue(event);
+    arrastrar(event) {
+        if (this.arrastrando) {
+            this.actualizarValor(event);
         }
     }
 
-    stopDrag() {
-        this.dragging = false;
+    detenerArrastre() {
+        this.arrastrando = false;
     }
 
-    updateValue(event) {
-        const rect = this.outerCircle.getBoundingClientRect();
+    actualizarValor(event) {
+        const rect = this.circuloExterior.getBoundingClientRect();
         const x = event.clientX - rect.left - rect.width / 2;
         const y = event.clientY - rect.top - rect.height / 2;
         let angle = Math.atan2(y, x) - Math.PI / 2;
@@ -61,35 +61,36 @@ class DeslizadorCircular {
         if (angle >= 27 * Math.PI / 15) angle = 27 * Math.PI / 15;
 
         // Calcular valor basado en el ángulo
-        this.value = ((angle) / (2 * Math.PI)) * 100;
-        if (this.value < 0) this.value = 0;
-        if (this.value > 100) this.value = 100;
+        this.valor = ((angle) / (2 * Math.PI)) * 100;
+        if (this.valor < 0) this.valor = 0;
+        if (this.valor > 100) this.valor = 100;
 
-        console.log(`Angle: ${angle}, Slider Value: ${this.value}`);
+        console.log(`Angle: ${angle}, Slider Value: ${this.valor}`);
 
-        this.draw();
-        this.emitValueChange();
+        this.dibujar();
+        this.emitirCambioValor();
     }
 
-    emitValueChange() {
+    emitirCambioValor() {
+        if (!isFinite(this.valor)) return; // Verificar si el valor es finito
         const event = new CustomEvent('valuechange', {
             detail: {
                 id: this.id,
-                value: this.value
+                value: this.valor
             }
         });
         document.dispatchEvent(event);
     }
 
-    draw() {
-        const angle = ((this.value / 100) * 2 * Math.PI + Math.PI / 2);
-        this.indicator.style.transform = `translate(0%, -50%) rotate(${angle}rad)`;
+    dibujar() {
+        const angle = ((this.valor / 100) * 2 * Math.PI + Math.PI / 2);
+        this.indicador.style.transform = `translate(0%, -50%) rotate(${angle}rad)`;
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const sliders = document.querySelectorAll('.deslizador-circular');
-    sliders.forEach(slider => {
-        new DeslizadorCircular(slider, { value: 10, id: slider.id });
+    const deslizadores = document.querySelectorAll('.deslizador-circular');
+    deslizadores.forEach(deslizador => {
+        new DeslizadorCircular(deslizador, { value: 10, id: deslizador.id });
     });
 });
--- scripts.js
+++ scripts.js
@@ -1,51 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const audioButtons = document.querySelectorAll('.selector');
-    document.getElementById('reproducir').addEventListener('click', function() {
-        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        console.log('AudioContext created or resumed:', audioContext);
-    });
-    
-    const audioSources = {};
+    const botonesAudio = document.querySelectorAll('.selector');
+    const contextoAudio = new (window.AudioContext || window.webkitAudioContext)();
+    const fuentesAudio = {};
+    let consolaEncendida = false;
 
-    function loadAudio(url) {
+    function cargarAudio(url) {
         return fetch(url)
             .then(response => response.arrayBuffer())
-            .then(buffer => audioContext.decodeAudioData(buffer));
+            .then(buffer => contextoAudio.decodeAudioData(buffer));
     }
 
-    function playAudio(audioBuffer) {
-        const source = audioContext.createBufferSource();
+    function reproducirAudio(audioBuffer) {
+        const source = contextoAudio.createBufferSource();
         source.buffer = audioBuffer;
         source.loop = true;
-        const gainNode = audioContext.createGain(); // Control de volumen
-        source.connect(gainNode).connect(audioContext.destination);
+        const gainNode = contextoAudio.createGain(); // Control de volumen
+        source.connect(gainNode).connect(contextoAudio.destination);
         source.start();
         return { source, gainNode }; // Devolvemos source y gainNode
     }
 
-    audioButtons.forEach((button, index) => {
+    botonesAudio.forEach((button) => {
         button.dataset.active = 'false';
         const audioUrl = button.getAttribute('data-audio');
         const sectionId = button.closest('.fila').dataset.section;
         button.setAttribute('data-section', sectionId);
 
-        loadAudio(audioUrl).then(audioBuffer => {
-            audioSources[button.id] = {
+        cargarAudio(audioUrl).then(audioBuffer => {
+            fuentesAudio[button.id] = {
                 buffer: audioBuffer,
                 source: null,
                 gainNode: null // Inicialmente null
             };
 
             button.addEventListener('click', function() {
+                if (!consolaEncendida) return; // Si la consola está apagada, no hacer nada
+
                 if (this.classList.contains('active')) {
                     this.classList.remove('active');
                     this.dataset.active = 'false';
-                    audioSources[this.id].source.stop();
-                    audioSources[this.id].source = null;
+                    fuentesAudio[this.id].source.stop();
+                    fuentesAudio[this.id].source = null;
                 } else {
-                    const { source, gainNode } = playAudio(audioSources[this.id].buffer);
-                    audioSources[this.id].source = source;
-                    audioSources[this.id].gainNode = gainNode; // Guardamos el gainNode
+                    const { source, gainNode } = reproducirAudio(fuentesAudio[this.id].buffer);
+                    fuentesAudio[this.id].source = source;
+                    fuentesAudio[this.id].gainNode = gainNode; // Guardamos el gainNode
                     this.classList.add('active');
                     this.dataset.active = 'true';
                 }
@@ -57,10 +56,93 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('valuechange', (event) => {
         const { id, value } = event.detail;
         const gainValue = value / 100;
-        Object.keys(audioSources).forEach(key => {
-            if (audioSources[key].source && document.querySelector(`#${key}`).dataset.section === id) {
-                audioSources[key].gainNode.gain.setValueAtTime(gainValue, audioContext.currentTime);
+        if (!isFinite(gainValue)) return; // Verificar si gainValue es finito
+        Object.keys(fuentesAudio).forEach(key => {
+            if (fuentesAudio[key].source && document.querySelector(`#${key}`).dataset.section === id) {
+                fuentesAudio[key].gainNode.gain.setValueAtTime(gainValue, contextoAudio.currentTime);
             }
         });
     });
+
+    // Función para actualizar el estado de los botones de silencio y solo
+    function actualizarEstadoAudio() {
+        const botonesSilencio = document.querySelectorAll('.mute');
+        const botonesSolo = document.querySelectorAll('.solo');
+        const seccionesActivas = Array.from(botonesSolo).filter(btn => btn.classList.contains('activo')).map(btn => btn.id.replace('solo-', ''));
+
+        botonesSilencio.forEach(btn => {
+            const sectionId = btn.id.replace('mute-', '');
+            const gainNodes = Array.from(document.querySelectorAll(`.selector[data-section="volumen-${sectionId}"]`)).map(selector => fuentesAudio[selector.id]?.gainNode);
+
+            if (btn.classList.contains('activo')) {
+                gainNodes.forEach(gainNode => {
+                    if (gainNode) gainNode.gain.setValueAtTime(0, contextoAudio.currentTime);
+                });
+            } else if (seccionesActivas.length > 0 && !seccionesActivas.includes(sectionId)) {
+                gainNodes.forEach(gainNode => {
+                    if (gainNode) gainNode.gain.setValueAtTime(0, contextoAudio.currentTime);
+                });
+            } else {
+                gainNodes.forEach(gainNode => {
+                    if (gainNode) gainNode.gain.setValueAtTime(1, contextoAudio.currentTime);
+                });
+            }
+        });
+    }
+
+    // Agregar funcionalidad a los botones de silencio y solo
+    const botonesSilencio = document.querySelectorAll('.mute');
+    const botonesSolo = document.querySelectorAll('.solo');
+
+    botonesSilencio.forEach(btn => {
+        btn.addEventListener('click', function() {
+            if (!consolaEncendida) return; // Si la consola está apagada, no hacer nada
+            this.classList.toggle('activo');
+            actualizarEstadoAudio();
+        });
+    });
+
+    botonesSolo.forEach(btn => {
+        btn.addEventListener('click', function() {
+            if (!consolaEncendida) return; // Si la consola está apagada, no hacer nada
+            this.classList.toggle('activo');
+            actualizarEstadoAudio();
+        });
+    });
+
+    // Agregar funcionalidad al botón encender
+    const botonEncender = document.getElementById('encender');
+
+    botonEncender.addEventListener('click', function() {
+        consolaEncendida = !consolaEncendida;
+        if (consolaEncendida) {
+            botonEncender.classList.add('activo');
+            botonEncender.querySelector('i').style.color = 'black';
+        } else {
+            botonEncender.classList.remove('activo');
+            botonEncender.querySelector('i').style.color = 'red';
+            // Detener todos los audios si la consola se apaga
+            Object.keys(fuentesAudio).forEach(key => {
+                if (fuentesAudio[key].source) {
+                    fuentesAudio[key].source.stop();
+                    fuentesAudio[key].source = null;
+                }
+            });
+            // Desactivar todos los botones de audio
+            botonesAudio.forEach(button => {
+                button.classList.remove('active');
+                button.dataset.active = 'false';
+            });
+            // Desactivar todos los botones de silencio y solo
+            botonesSilencio.forEach(button => {
+                button.classList.remove('activo');
+            });
+            botonesSolo.forEach(button => {
+                button.classList.remove('activo');
+            });
+        }
+    });
+
+    // Inicializar color del icono en rojo
+    botonEncender.querySelector('i').style.color = 'red';
 });
